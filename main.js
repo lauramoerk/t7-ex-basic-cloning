3e0849dfe6ad7a956103dc046c8e38b8118b4a8d
lauramoerk <lauramoerk@hotmail.com>
2022-02-04 10:47:57 +0100

    superdoodle

--- main.js
+++ main.js
@@ -6,13 +6,24 @@ const gitHop = {
 };
 
 //Step 1: Chose the <template>'s content
+const templateElement = document.querySelector("#myBeerTemplate").content;
 
-//Step 2: Make a "clone"
+//Make a clone
+const myClone = templateElement.cloneNode(true);
 
 //Step 3: Change the content of the clone
+
+myClone.querySelector("h1").textContent = `${gitHop.name}`;
+myClone.querySelector("img").src = `${gitHop.imgSrc}`;
+myClone.querySelector("p").textContent = `${gitHop.description}`;
+
 //use the data from the object above (you can do this when the rest is working)
 
 //Step 4: Chose the new "parent" element
 //Append it to main
 
+const parentElement = document.querySelector("main");
+parentElement.appendChild(myClone);
+
 //Step 5: Add (Append) the clone to the DOM
+
